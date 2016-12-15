897e1ab855e151341b9147bf64f5fe55094023ce
CodingKnowledge <manjunatha.gunderi@gmail.com>
2016-12-15 13:04:02 -0500

    Add files via upload

---
+++ SpringDependencyInjectionByUsingAutowirewithSteroClass/src/com/codingKnowledge/entity/Car.java
@@ -0,0 +1,24 @@
+package com.codingKnowledge.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+	int modelYear;
+
+	@Qualifier(value="e2")
+	@Autowired
+	Engine engine;
+
+	public void setModelYear(int modelYear) {
+		this.modelYear = modelYear;
+	}
+
+	public void printCar() {
+		System.out.println("Model Year: " + modelYear + "\tEngine: " + engine.printEngine());
+	}
+
+}
---
+++ SpringDependencyInjectionByUsingAutowirewithSteroClass/src/com/codingKnowledge/entity/Engine.java
@@ -0,0 +1,21 @@
+package com.codingKnowledge.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Engine {
+	String engineType;
+
+	public void setEngineType(String engineType) {
+		this.engineType = engineType;
+	}
+
+	public String getEngineType() {
+		return engineType;
+	}
+
+	public String printEngine() {
+		return engineType;
+	}
+
+}
