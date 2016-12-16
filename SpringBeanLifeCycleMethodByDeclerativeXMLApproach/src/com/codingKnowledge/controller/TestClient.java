a4bcb728ccf6a869cbfb39349d59e92f09f8011f
CodingKnowledge <manjunatha.gunderi@gmail.com>
2016-12-16 16:21:19 -0500

    Add files via upload

---
+++ SpringBeanLifeCycleMethodByDeclerativeXMLApproach/src/com/codingKnowledge/controller/TestClient.java
@@ -0,0 +1,40 @@
+package com.codingKnowledge.controller;
+
+import java.util.Scanner;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.codingKnowledge.entity.Client;
+
+public class TestClient {
+	private static Scanner sc = null;
+
+	public static void main(String[] args) throws Exception {
+
+		ConfigurableApplicationContext cap = new ClassPathXmlApplicationContext("spring.xml");
+
+		while (true) {
+			sc = new Scanner(System.in);
+			System.out.println("Enter 1 to 'Save' or 2 to 'Close'....");
+			int key = sc.nextInt();
+			switch (key) {
+			case 1:
+				Client c = (Client) cap.getBean("client");
+				System.out.println("Enter the Client Id:");
+				int id = sc.nextInt();
+				System.out.println("Enter the Client Name:");
+				String name = sc.next();
+				System.out.println("Enter the Client Gender:");
+				String gender = sc.next();
+				System.out.println("Enter the Client Address:");
+				String address = sc.next();
+				c.save(id, name, gender, address);
+				break;
+			default:
+				cap.close();
+				break;
+			}
+		}
+	}
+}
