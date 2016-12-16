5699f3972b573d486b63a8e4ba46d4669c47376a
CodingKnowledge <manjunatha.gunderi@gmail.com>
2016-12-16 12:37:28 -0500

    Update VendorFactory.java

--- SpringObjectCreationByFactoryClassesStaticFactoryApproch/src/com/codingKnowledge/entity/VendorFactory.java
+++ SpringObjectCreationByFactoryClassesStaticFactoryApproch/src/com/codingKnowledge/entity/VendorFactory.java
@@ -7,7 +7,8 @@ public class VendorFactory {
 	public static void setProjectName(String projectName) {
 		VendorFactory.projectName = projectName;
 	}
-
+	
+	/* Static Factory Approach */
 	public static Vendor getInstance() throws Exception {
 
 		Vendor vendor = (Vendor) Class.forName(projectName).newInstance();
