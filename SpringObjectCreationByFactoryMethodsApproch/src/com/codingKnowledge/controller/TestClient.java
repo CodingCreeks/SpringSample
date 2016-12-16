611634107f7c4b5fa8ae1cddbbc39903067cc1ef
CodingKnowledge <manjunatha.gunderi@gmail.com>
2016-12-16 11:53:02 -0500

    Update TestClient.java

--- SpringObjectCreationByFactoryMethodsApproch/src/com/codingKnowledge/controller/TestClient.java
+++ SpringObjectCreationByFactoryMethodsApproch/src/com/codingKnowledge/controller/TestClient.java
@@ -16,11 +16,15 @@ public class TestClient {
 		Client client1 = (Client) context.getBean("cl");
 		Client client2 = (Client) context.getBean("cl");
 		
+		System.out.println(client1);
+		System.out.println(client2);
 		System.out.println(client1==client2);
 		
 		Calendar c1 = (Calendar) context.getBean("ca");
 		Calendar c2 = (Calendar) context.getBean("ca");
 		
+		System.out.println(c1);
+		System.out.println(c2);
 		System.out.println(c1==c2);
 		System.out.println(c1.getCalendarType());
 
