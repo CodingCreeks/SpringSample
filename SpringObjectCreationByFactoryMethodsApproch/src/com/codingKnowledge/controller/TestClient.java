7518629a0b8a320efac51af2fbe7611e8ef9d8af
CodingKnowledge <manjunatha.gunderi@gmail.com>
2016-12-16 11:59:54 -0500

    Update TestClient.java

--- SpringObjectCreationByFactoryMethodsApproch/src/com/codingKnowledge/controller/TestClient.java
+++ SpringObjectCreationByFactoryMethodsApproch/src/com/codingKnowledge/controller/TestClient.java
@@ -18,14 +18,14 @@ public class TestClient {
 		
 		System.out.println(client1);
 		System.out.println(client2);
-		System.out.println(client1==client2);
+		System.out.println("\nIs Client is Singleton ? : "+(client1==client2));
 		
 		Calendar c1 = (Calendar) context.getBean("ca");
 		Calendar c2 = (Calendar) context.getBean("ca");
 		
-		System.out.println(c1);
+		System.out.println("\n"+c1);
 		System.out.println(c2);
-		System.out.println(c1==c2);
+		System.out.println("\nIs Calendar is Singleton ? : "+(c1==c2));
 		System.out.println(c1.getCalendarType());
 
 	}
