9b59fdd50524fd32e5b0a01e2ae4f587d893c7c5
CodingKnowledge <manjunatha.gunderi@gmail.com>
2016-12-20 15:54:56 -0500

    Add files via upload

---
+++ MavenSpringMVCDemoWithAnnotation/src/main/java/com/codingKnowledge/controller/AppController.java
@@ -0,0 +1,51 @@
+package com.codingKnowledge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.codingKnowledge.model.AppUser;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("index.sp")
+	public ModelAndView indexPage() {
+
+		System.out.println("Inside IndexPage method.....");
+		
+		/*String msg = "CodingKnowledge";*/
+		ModelAndView mv = new ModelAndView("index");
+		
+		/*mv.addObject("message", msg);*/
+		mv.addObject("message", "CodingKnowledge tutorials");
+
+		return mv;
+	}
+
+	@RequestMapping("applogin.sp")
+	public ModelAndView appLogin() {
+
+		System.out.println("Inside applogin method.....");
+
+		AppUser appuser = new AppUser();
+		appuser.setName("Manju");
+		appuser.setEmail("manju@gmail.com");
+
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("applogin");
+		mv.addObject("apploginform", appuser);
+
+		/* return new ModelAndView("applogin", "apploginform", appuser); */
+		return mv;
+	}
+
+	@RequestMapping("/appusersearch.sp")
+	public String appUserSearch() {
+		System.out.println("Inside appusersearch method.....");
+
+		return "index";
+
+	}
+
+}
---
+++ MavenSpringMVCDemoWithAnnotation/src/main/java/com/codingKnowledge/model/AppUser.java
@@ -0,0 +1,23 @@
+package com.codingKnowledge.model;
+
+public class AppUser {
+
+	String name;
+	String email;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+}
